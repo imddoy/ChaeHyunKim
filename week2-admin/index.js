dc13956823131817f49d9f4c260d462c7f20e0fa
imddoy <imddoy@duksung.ac.kr>
2024-10-23 21:45:04 +0900

    refactor: 데이터 렌더링 함수 분리

--- week2-admin/index.js
+++ week2-admin/index.js
@@ -6,36 +6,49 @@ if (!localStorage.getItem("members")) {
   localStorage.setItem("members", JSON.stringify(members));
 }
 
-const nowMembers = JSON.parse(localStorage.getItem("members"));
+// 멤버 데이터를 로컬 스토리지에서 가져오는 함수
+const getMembers = () => {
+  return JSON.parse(localStorage.getItem("members"));
+};
 
-// 테이블 본문 가져오기
-const tableBody = document.querySelector("tbody");
+// 테이블 렌더링하는 함수
+const renderTable = () => {
+  const nowMembers = getMembers();
 
-// DocumentFragment 생성
-const fragment = document.createDocumentFragment();
+  // 테이블 초기화
+  const tableBody = document.querySelector("tbody");
+  tableBody.textContent = "";
 
-// 템플릿 가져오기
-const template = document.querySelector("#row-template");
+  // DocumentFragment 생성
+  const fragment = document.createDocumentFragment();
 
-// 데이터 순회하며 행 생성 및 추가
-nowMembers.forEach((member) => {
-  // template에서 복제된 내용 가져오기
-  const clone = template.content.cloneNode(true);
+  // 템플릿 가져오기
+  const template = document.querySelector("#row-template");
 
-  // 복제된 행의 각 셀에 데이터 채우기
-  clone.querySelector(".name").textContent = member.name;
-  clone.querySelector(".english-name").textContent = member.englishName;
-  clone.querySelector(".github").textContent = member.github;
-  clone.querySelector(".gender").textContent =
-    member.gender === "male" ? "남자" : "여자";
-  clone.querySelector(".role").textContent = member.role;
-  clone.querySelector(".first-week-group").textContent = member.firstWeekGroup;
-  clone.querySelector(".second-week-group").textContent =
-    member.secondWeekGroup;
+  // 데이터 순회하며 행 생성 및 추가
+  nowMembers.forEach((member) => {
+    // template에서 복제된 내용 가져오기
+    const clone = template.content.cloneNode(true);
 
-  // DocumentFragment에 행 추가
-  fragment.appendChild(clone);
-});
+    // 복제된 행의 각 셀에 데이터 채우기
+    clone.querySelector(".name").textContent = member.name;
+    clone.querySelector(".english-name").textContent = member.englishName;
+    clone.querySelector(".github").textContent = member.github;
+    clone.querySelector(".gender").textContent =
+      member.gender === "male" ? "남자" : "여자";
+    clone.querySelector(".role").textContent = member.role;
+    clone.querySelector(".first-week-group").textContent =
+      member.firstWeekGroup;
+    clone.querySelector(".second-week-group").textContent =
+      member.secondWeekGroup;
 
-// 테이블에 DocumentFragment 추가
-tableBody.appendChild(fragment);
+    // DocumentFragment에 행 추가
+    fragment.appendChild(clone);
+  });
+
+  // 테이블에 DocumentFragment 추가
+  tableBody.appendChild(fragment);
+};
+
+// 페이지가 로드될 때 테이블 렌더링
+renderTable();
