825812a752916c4432df599b69c96da1c44182ec
imddoy <imddoy@duksung.ac.kr>
2024-11-12 23:15:22 +0900

    design: 버튼 hover 시 transition 배경색 전환 적용

--- week4-user/src/components/common/button/Button.style.ts
+++ week4-user/src/components/common/button/Button.style.ts
@@ -6,10 +6,15 @@ export const Button = styled.button`
   padding: 0 1.6rem;
 
   background-color: ${({ theme, disabled }) =>
-    disabled ? theme.colors.gray_700 : theme.colors.main_pink_400};
+    disabled ? theme.colors.pink_900 : theme.colors.main_pink_400};
   color: ${({ theme }) => theme.colors.white};
   ${({ theme }) => theme.fonts["body2-normal-medi"]};
   border-radius: 6px;
-
+  transition: background-color 0.5s;
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+
+  &: hover {
+    background-color: ${({ theme, disabled }) =>
+      disabled ? theme.colors.pink_900 : theme.colors.pink_600};
+  }
 `;
